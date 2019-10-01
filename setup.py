9a411d09b30432b2154d2ea85d23eb11946f86c4
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-10-01 18:47:52 -0400

    updated docstrings and fixed formatting

--- setup.py
+++ setup.py
@@ -1,30 +1,33 @@
 '''
-	Created on July 9, 2016
-	@author: Andrew Abi-Mansour
+Setup file for PyGran.analysis
 
-	This is the 
-	 __________         ________                     
-	██████╗ ██╗   ██╗ ██████╗ ██████╗  █████╗ ███╗   ██╗
-	██╔══██╗╚██╗ ██╔╝██╔════╝ ██╔══██╗██╔══██╗████╗  ██║
-	██████╔╝ ╚████╔╝ ██║  ███╗██████╔╝███████║██╔██╗ ██║
-	██╔═══╝   ╚██╔╝  ██║   ██║██╔══██╗██╔══██║██║╚██╗██║
-	██║        ██║   ╚██████╔╝██║  ██║██║  ██║██║ ╚████║
-	╚═╝        ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
-																											
-	DEM simulation and analysis toolkit
-	http://www.pygran.org, support@pygran.org
+Created on October 10, 2019
 
-	Core developer and main author:
-	Andrew Abi-Mansour, andrew.abi.mansour@pygran.org
+Author: Andrew Abi-Mansour
 
-	PyGran is open-source, distributed under the terms of the GNU Public
-	License, version 2 or later. It is distributed in the hope that it will
-	be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-	of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have
-	received a copy of the GNU General Public License along with PyGran.
-	If not, see http://www.gnu.org/licenses . See also top-level README
-	and LICENSE files.
- '''
+This is the::
+
+        ██████╗ ██╗   ██╗ ██████╗ ██████╗  █████╗ ███╗   ██╗
+        ██╔══██╗╚██╗ ██╔╝██╔════╝ ██╔══██╗██╔══██╗████╗  ██║
+        ██████╔╝ ╚████╔╝ ██║  ███╗██████╔╝███████║██╔██╗ ██║
+        ██╔═══╝   ╚██╔╝  ██║   ██║██╔══██╗██╔══██║██║╚██╗██║
+        ██║        ██║   ╚██████╔╝██║  ██║██║  ██║██║ ╚████║
+        ╚═╝        ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+
+DEM simulation and analysis toolkit
+http://www.pygran.org, support@pygran.org
+
+Core developer and main author:
+Andrew Abi-Mansour, andrew.abi.mansour@pygran.org
+
+PyGran is open-source, distributed under the terms of the GNU Public
+License, version 2 or later. It is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have
+received a copy of the GNU General Public License along with PyGran.
+If not, see http://www.gnu.org/licenses . See also top-level README
+and LICENS
+'''
 
 from setuptools import setup, find_packages
 import glob, shutil, re
@@ -43,34 +46,35 @@ except:
 	include_dirs = []
 
 setup(
-		name = "PyGran.analysis",
-		version = __version__,
-		author = __author__,
-		author_email = __email__,
-		description = ("A DEM toolkit for rapid quantitative analysis of granular/powder systems"),
-		license = "GNU v2",
-		keywords = "Discrete Element Method, Granular Materials",
-		url = "https://github.com/Andrew-AbiMansour/PyGran",
-		packages=find_packages(),
-		include_package_data=True,
-		install_requires=['numpy', 'scipy'],
-		extras_require={'extra': ['vtk', 'Pillow']},
-		long_description='A DEM toolbox for rapid quantitative analysis of granular/powder systems. See http://www.pygran.org.',
-		classifiers=[
-				"Development Status :: 4 - Beta",
-				"Topic :: Utilities",
-				"License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-				"Programming Language :: Python :: 2.7",
-				"Programming Language :: Python :: 3",
-				"Programming Language :: Python :: 3.4",
-				"Programming Language :: Python :: 3.5",
-				"Programming Language :: Python :: 3.6",
-				"Programming Language :: Python :: 3.7",
-				"Programming Language :: C",
-				"Operating System :: POSIX :: Linux"
-		],
+	name = "PyGran.analysis",
+	version = __version__,
+	author = __author__,
+	author_email = __email__,
+	description = ("A submodule part of PyGran toolkit for rapid quantitative analysis of granular/powder systems"),
+	license = "GNU v2",
+	keywords = "Discrete Element Method, Granular Materials",
+	url = "https://github.com/Andrew-AbiMansour/PyGran",
+	packages=find_packages(),
+	include_package_data=True,
+	install_requires=['numpy', 'scipy'],
+	extras_require={'extra': ['vtk', 'Pillow']},
+	long_description='A submodule part of PyGran toolkit for rapid quantitative analysis of granular/powder systems.' + \
+			' See http://www.pygran.org for more info on PyGran.',
+	classifiers=[
+			"Development Status :: 4 - Beta",
+			"Topic :: Utilities",
+			"License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+			"Programming Language :: Python :: 2.7",
+			"Programming Language :: Python :: 3",
+			"Programming Language :: Python :: 3.4",
+			"Programming Language :: Python :: 3.5",
+			"Programming Language :: Python :: 3.6",
+			"Programming Language :: Python :: 3.7",
+			"Programming Language :: C",
+			"Operating System :: POSIX :: Linux"
+	],
 
-		zip_safe=False,
-		ext_modules=optimal_list,
-		include_dirs=include_dirs
+	zip_safe=False,
+	ext_modules=optimal_list,
+	include_dirs=include_dirs
 )
