0c3ab91cd9a768c67b6b4f92f1e00064a16121b1
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-10-27 00:24:13 -0400

    fixed docstring

--- PyGranAnalysis/contacts.py
+++ PyGranAnalysis/contacts.py
@@ -1,37 +1,35 @@
-#  -*- coding: utf8 -*-
+'''
+Created on March 10, 2017
+Author: Andrew Abi-Mansour
+
+This is the::
+__________         ________                     
+██████╗ ██╗   ██╗ ██████╗ ██████╗  █████╗ ███╗   ██╗
+██╔══██╗╚██╗ ██╔╝██╔════╝ ██╔══██╗██╔══██╗████╗  ██║
+██████╔╝ ╚████╔╝ ██║  ███╗██████╔╝███████║██╔██╗ ██║
+██╔═══╝   ╚██╔╝  ██║   ██║██╔══██╗██╔══██║██║╚██╗██║
+██║        ██║   ╚██████╔╝██║  ██║██║  ██║██║ ╚████║
+╚═╝        ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+                                                  
+DEM simulation and analysis toolkit
+http://www.pygran.org, support@pygran.org
+
+Core developer and main author:
+Andrew Abi-Mansour, andrew.abi.mansour@pygran.org
+
+PyGran is open-source, distributed under the terms of the GNU Public
+License, version 2 or later. It is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have
+received a copy of the GNU General Public License along with PyGran.
+If not, see http://www.gnu.org/licenses . See also top-level README
+and LICENSE files.
+
+-------------------------------------------------------------------------
+Python module for determining contacts/overlaps in an N-particle system
+-------------------------------------------------------------------------
 
 '''
-  Created on March 10, 2017
-  @author: Andrew Abi-Mansour
-
-  This is the 
-   __________         ________                     
-  ██████╗ ██╗   ██╗ ██████╗ ██████╗  █████╗ ███╗   ██╗
-  ██╔══██╗╚██╗ ██╔╝██╔════╝ ██╔══██╗██╔══██╗████╗  ██║
-  ██████╔╝ ╚████╔╝ ██║  ███╗██████╔╝███████║██╔██╗ ██║
-  ██╔═══╝   ╚██╔╝  ██║   ██║██╔══██╗██╔══██║██║╚██╗██║
-  ██║        ██║   ╚██████╔╝██║  ██║██║  ██║██║ ╚████║
-  ╚═╝        ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
-                                                      
-  DEM simulation and analysis toolkit
-  http://www.pygran.org, support@pygran.org
-
-  Core developer and main author:
-  Andrew Abi-Mansour, andrew.abi.mansour@pygran.org
-
-  PyGran is open-source, distributed under the terms of the GNU Public
-  License, version 2 or later. It is distributed in the hope that it will
-  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have
-  received a copy of the GNU General Public License along with PyGran.
-  If not, see http://www.gnu.org/licenses . See also top-level README
-  and LICENSE files.
-
- -------------------------------------------------------------------------
-  Python module for determining contacts/overlaps in an N-particle system
- -------------------------------------------------------------------------
-
- '''
 
 import numpy
 from scipy import spatial
