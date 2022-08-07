93244b616827794427f96d2346514d49830b133f
andrew-abimansour <andrew.gaam@gmail.com>
2022-08-07 00:16:52 -0400

    add fixture for mesh files

--- pygran_analysis/tests/conftest.py
+++ pygran_analysis/tests/conftest.py
@@ -3,8 +3,14 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption("--trajf", action="store", help="trajectory filename")
+    parser.addoption("--meshf", action="store", help="mesh filename")
 
 
 @pytest.fixture
 def trajf(request):
     return request.config.getoption("--trajf")
+
+
+@pytest.fixture
+def meshf(request):
+    return request.config.getoption("--meshf")
